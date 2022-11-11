722a2d2eba6f5ad94360991385e34aa310923d3c
Natoshia Dumont <natoshia.dumont@gmail.com>
2022-11-10 23:25:25 -0600

    ALERT: THE WORM IS MOVING! REPEAT: THE WORM IS MOVING!

--- app.js
+++ app.js
@@ -8,55 +8,17 @@ wormColor -> palevioletred
 let gameGrid = document.getElementById('game-grid');
 let restartButton = document.getElementById('restart-game');
 let worm = [
-   {x:10, y:11},
+   {x:13, y:11},
+   {x:12, y:11},
    {x:11, y:11},
-   {x:12, y:11}
+   {x:10, y:11},
+   {x:9, y:11},
 ]
 const wormSpeed = 1;
 let score = 0;
 
 // state
 let state;
-
-//HELPER FUNCTIONS
-
-//function shoudl update worm with each render
-function moveWorm(){
-   gameGrid.innerHTML = ''
-   //console.log('updateWorm') -> this puppy is WORKING
-   //use for loop to iterate over worm from second to last element, since last element will disapear when moving
-   for(let i = worm.lenth -2; i >= 0; i--){
-      worm[i +1] = {...worm[i]}
-
-   }
-   worm[0].x += 1;
-   worm[0].y += 0;
-
-}
-//create function to set second snack unit to a darker pink
-
-//function should represrent what the worm looks like when rendered in html
-function renderWorm(wormGrid){
-   //console.log('makeWorm'); // -> this puppy is WORKING
-   worm.forEach(singleSquare =>{
-      const wormElement = document.createElement('div');
-      wormElement.style.gridRowStart = singleSquare.y;
-      wormElement.style.gridColumnStart = singleSquare.x;
-      wormElement.classList.add('worm');
-      wormGrid.appendChild(wormElement);
-   })
-  
-}
-
-
-
-
-//setting initial state of game before pressing 'play'
-function buildInitialState() {
-
-}
-buildInitialState();
-
 
 //create a variable that captures the last time the screen image was rendered
 let lastRender = 0;
@@ -67,18 +29,66 @@ function renderState(currentTime) {
    if(lastRenderTime < 1 / wormSpeed){
       return;
    }
-   console.log('rendering');
+  //console.log('rendering');
    lastRender = currentTime;
+
    moveWorm();
-   renderWorm(gameGrid);
+   drawWorm();
 }
 window.requestAnimationFrame(renderState);
 
 
+//HELPER FUNCTIONS
+
+//function shoudl update worm with each render
+function moveWorm(){
+   //console.log('updateWorm') -> this puppy is WORKING
+   //use for loop to iterate over worm from second to last element, since last element will disapear when moving
+   for(let i = worm.length - 2; i >= 0; i--){
+      worm[i + 1] = { ...worm[i] }
+      
+   }
+   worm[0].x += 1;
+   worm[0].y += 0;
+}
+//create function to set second snack unit to a darker pink
+
+//function should represrent what the worm looks like when rendered in html
+function drawWorm(){
+   gameGrid.innerHTML = '';
+   renderWorm(gameGrid);
+}
+
+function renderWorm(wormGrid){
+   //console.log('makeWorm'); // -> this puppy is WORKING
+   worm.forEach(square =>{
+      const wormElement = document.createElement('div');
+      wormElement.style.gridRowStart = square.y;
+      wormElement.style.gridColumnStart = square.x;
+      
+      if(square === worm[1] || square === worm[2]){
+         wormElement.classList.add('alternateWormClass');
+      }
+      else{
+         wormElement.classList.add('worm');
+      }
+      wormGrid.appendChild(wormElement);
+   })
+
+}
+
+
+
+//setting initial state of game before pressing 'play'
+function buildInitialState() {
+
+}
+buildInitialState();
+
 
 // listeners
 restartButton.addEventListener('click', ()=>{
-   document.location.reload();
+   location.reload();
 })
 function onBoardClick() {
   // update state, maybe with another dozen or so helper functions...
