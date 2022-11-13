17e160c02da2511d65460f296a5be5d1bc806f93
Natoshia Dumont <natoshia.dumont@gmail.com>
2022-11-12 18:26:12 -0600

    snake movement debugging

--- app.js
+++ app.js
@@ -8,15 +8,13 @@ wormColor -> palevioletred
 let gameGrid = document.getElementById('game-grid');
 let restartButton = document.getElementById('restart-game');
 let worm = [
-   {x:13, y:11},
-   {x:12, y:11},
    {x:11, y:11},
-   {x:10, y:11},
-   {x:9, y:11},
 ]
-const wormSpeed = 1;
+const wormSpeed = 3;
 let score = 0;
-
+// make move const that has initial movement change of 0:0
+let moveState = {x:0, y:0};
+let lastMoveState = {x:0, y:0};
 // state
 let state;
 
@@ -40,24 +38,65 @@ window.requestAnimationFrame(renderState);
 
 //HELPER FUNCTIONS
 
-//function shoudl update worm with each render
+
+
+//create function to set second snack unit to a darker pink
+function drawWorm(){
+   gameGrid.innerHTML = '';
+   renderWorm(gameGrid);
+}
+
+//add event listener that tracks pressing the arro keys.
+window.addEventListener('keydown', e =>{
+   //update: create conditional to avoid moving worm in on itself.
+   //BUG: worm can move in direction that goes on top of itself. ugh
+   // here you might read which key was pressed and update the state accordingly
+   //make switch cases to update worm direction
+   switch(e.key){
+      case 'ArrowUp':
+         if(lastMoveState.y !== 0){break;}
+         moveState = {x:0, y:-1};
+         break; 
+      case 'ArrowDown':
+         if(lastMoveState.y !== 0){break;}
+      moveState = {x:0, y:1}
+         break; 
+      case 'ArrowLeft':
+         if(lastMoveState.x !== 0){break;}
+         moveState = {x:-1, y:0}
+         break; 
+      case 'ArrowRight':
+         if(lastMoveState.x !== 0){break;}
+         moveState = {x:1, y:0}
+         break; 
+   }
+})
+
+
+function movementFunc(){
+   lastMoveState = moveState;
+   return moveState;
+}
+
+
+//moveWorm function should handle worm movements on grid
+//function should update worm with each render and have all sections following
 function moveWorm(){
+   const direction = moveState;
    //console.log('updateWorm') -> this puppy is WORKING
    //use for loop to iterate over worm from second to last element, since last element will disapear when moving
    for(let i = worm.length - 2; i >= 0; i--){
       worm[i + 1] = { ...worm[i] }
       
    }
-   worm[0].x += 1;
-   worm[0].y += 0;
+   worm[0].x += direction.x;
+   worm[0].y += direction.y;
+   movementFunc();
 }
-//create function to set second snack unit to a darker pink
+
+
 
 //function should represrent what the worm looks like when rendered in html
-function drawWorm(){
-   gameGrid.innerHTML = '';
-   renderWorm(gameGrid);
-}
 
 function renderWorm(wormGrid){
    //console.log('makeWorm'); // -> this puppy is WORKING
@@ -77,8 +116,6 @@ function renderWorm(wormGrid){
 
 }
 
-
-
 //setting initial state of game before pressing 'play'
 function buildInitialState() {
 
@@ -90,6 +127,8 @@ buildInitialState();
 restartButton.addEventListener('click', ()=>{
    location.reload();
 })
+
+
 function onBoardClick() {
   // update state, maybe with another dozen or so helper functions...
 
@@ -107,9 +146,13 @@ function tick() {
  
  setInterval(tick, 1000 / 30) // as close to 30 frames per second as possible
  
+
+ //event listener for arrow entires
+
+ 
+
+ 
  // now you might have things like
- document.addEventListener('keydown', function (event) {
-   // here you might read which key was pressed and update the state accordingly
- })
+
 
 
